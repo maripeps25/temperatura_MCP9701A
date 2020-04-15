9c72b659ba76ad74ada2ecfeccaedb3419fca7e1
maripeps25 <62264669+maripeps25@users.noreply.github.com>
2020-04-14 19:04:19 -0500

    Agregar archivos iniciales

---
+++ temperatura_MCP9701.ino
@@ -0,0 +1,29 @@
+
+
+//Constantes
+const float vout0 = 400;   //  Voltaje de salida del sensor en mV a 0°C
+const float tc = 19.53;   // mV por °C MCP9701/A
+
+// Variables
+int i, f;
+float vout, vout_avg, ta;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);           
+  analogReference(DEFAULT);
+}
+
+void loop() {
+  vout_avg = 0;
+  for (i = 0; i < 1024; i++) {                    //Ciclo para obtener promedio
+    vout = analogRead(A0) * (4976.30 / 1023);
+    vout_avg = vout_avg + vout;
+  }
+  vout = vout_avg / 1024;
+  ta = (vout - vout0) / tc;
+  Serial.print("Temperatura(C) = " );
+  Serial.println(ta);
+  delay (1000);
+}
